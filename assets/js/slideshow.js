1e637973baf3de221e14c4fd569e06310a3ab5f2
Victor Joseph <VBjoe4info@gmail.com>
2022-11-04 13:42:05 +0100

    white apparel

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -5,4 +5,4 @@ let blueIndex=0;function blueCarousel(){var e,l=document.getElementsByClassName(
 // tan apparel
 let tanIndex=0;function tanCarousel(){var e,n=document.getElementsByClassName("tanSlides");for(e=0;e<n.length;e++)n[e].style.display="none";tanIndex++,tanIndex>n.length&&(tanIndex=1),n[tanIndex-1].style.display="block",setTimeout(tanCarousel,1e3)}tanCarousel();
 // white apparel
-// let whiteIndex=0;function whiteCarousel(){var e,t=document.getElementsByClassName("whiteSlides");for(e=0;e<t.length;e++)t[e].style.display="none";whiteIndex++,whiteIndex>t.length&&(whiteIndex=1),t[whiteIndex-1].style.display="block",setTimeout(whiteCarousel,1e3)}whiteCarousel();
+let whiteIndex=0;function whiteCarousel(){var e,t=document.getElementsByClassName("whiteSlides");for(e=0;e<t.length;e++)t[e].style.display="none";whiteIndex++,whiteIndex>t.length&&(whiteIndex=1),t[whiteIndex-1].style.display="block",setTimeout(whiteCarousel,1e3)}whiteCarousel();
