9e8099febda62a0baed01d6a0ec4516b61aa1b54
Victor Joseph <VBjoe4info@gmail.com>
2022-11-04 13:22:01 +0100

    update

---
+++ assets/js/imageDrag.js
@@ -0,0 +1,36 @@
+// image highlight
+let featuredStrains = document.querySelectorAll(".featured-strains__container");
+featuredStrains.forEach(function (strain) {
+  strain.addEventListener("click", function () {
+    featuredStrains.forEach(function (strain) {
+      strain.classList.remove("active");
+    });
+    strain.classList.toggle("active");
+  });
+});
+
+// grab and scroll images
+const slider = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
