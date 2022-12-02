cb2da16f263f79ea1e67e20aef738f2c291c047d
jeremy0x <aworetanjeremiah@gmail.com>
2022-12-02 16:01:55 +0100

    fix: update page transition screen colors

--- assets/js/page-reveal.js
+++ assets/js/page-reveal.js
@@ -3,11 +3,11 @@ let tl = gsap.timeline({});
 tl.fromTo(
   ".hero-container",
   {
-    clipPath: "circle(0%)",
+    clipPath: "circle(7%)",
   },
   {
     clipPath: "circle(75%)",
     duration: 1.5,
-    delay: 2,
+    delay: 5,
   }
 );
