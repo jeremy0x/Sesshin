9cdd780e74bc60d1c8e4dab6f066b146b88e5fa0
Jeremy <aworetanjeremiah@gmail.com>
2023-01-05 09:11:53 +0100

    Implement requested changes from website review

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -5,8 +5,5 @@ var swiper = new Swiper('.mySwiper', {
     el: '.swiper-pagination',
     dynamicBullets: true,
     clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
-    },
   },
 });
